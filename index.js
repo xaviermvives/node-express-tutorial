2f30bb4cca0301656a27ce21cab47483742ad924
Xavier <montjuic79@gmail.com>
2022-05-15 09:28:05 +0200

    GET endpoint

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const server = express();
 
 const PORT = 5000;
 
+server.get("/", (req, res) => {
+  //   res.send("Hello world!");
+  res.json({ hello: "world" });
+});
+
 server.listen(PORT, () => {
   console.log(`\n*** Server running on htt://localhost:${PORT}`);
 });
