9b15de426e603c70f43d05d84468eece62f8f132
Xavier <montjuic79@gmail.com>
2022-05-16 09:27:24 +0200

    POST endpoint

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const shortId = require("shortid");
 
 const server = express();
+
+server.use(express.json());
+
+const channels = [];
+const lessons = [];
 
 const PORT = 5000;
 
 server.get("/", (req, res) => {
   //   res.send("Hello world!");
   res.json({ hello: "world" });
+});
+
+server.post("/api/channels", (req, res) => {
+  const channelInfo = req.body;
+  channelInfo.id = shortId.generate();
+  channels.push(channelInfo);
+  res.status(201).json(channelInfo);
+});
+
+server.get("/api/channels", (req, res) => {
+  res.status(200).json(channels);
 });
 
 server.listen(PORT, () => {
